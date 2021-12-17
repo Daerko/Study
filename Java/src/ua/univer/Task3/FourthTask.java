ae57eb5c89019b4229d696854b7b69492d1ef64c
Daerko <93711393+Daerko@users.noreply.github.com>
2021-12-17 13:39:39 +0200

    Add files via upload

---
+++ Java/src/ua/univer/Task3/FourthTask.java
@@ -0,0 +1,31 @@
+package ua.univer.Task3;
+
+import java.util.Scanner;
+
+public class FourthTask {
+
+    public static void main(String[] args) {
+        int a = getNumberInConsole(); // first element
+        double t = getDoubleInConsole(); // progression step
+        int alim = getNumberInConsole(); // limit
+        int result = symGeometricElements(a, t, alim);
+        System.out.println(result);
+    }
+
+    private static double getDoubleInConsole() {
+        return new Scanner(System.in).nextDouble();
+    }
+
+    private static int getNumberInConsole() {
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static int symGeometricElements(int a, double t, int alim) {
+        if(a < alim){
+            return 0;
+        }
+        else{
+            return a + symGeometricElements((int)Math.round(a*t),t, alim);
+        }
+    }
+}
---
+++ Java/src/ua/univer/Task3/ThirdTask.java
@@ -0,0 +1,27 @@
+package ua.univer.Task3;
+
+import java.util.Scanner;
+
+public class ThirdTask {
+
+    public static int MultiArithmeticElements(int a, int b){
+        int result = a;
+        int fN = a;
+        for (int k = 0; k <= b-2; k++) {
+            fN += 3;
+            result *= fN;
+            System.out.print(result + "  ");
+            System.out.print(fN + "  ");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the first number of progression: ");
+        int fNumber = in.nextInt();
+        System.out.print("Enter the count of numbers: ");
+        int cNumber = in.nextInt();
+        System.out.print("The result of progression: " + MultiArithmeticElements(fNumber, cNumber));
+    }
+}
