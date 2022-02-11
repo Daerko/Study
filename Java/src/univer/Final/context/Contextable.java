7e5f1689aefb4910d6de3ead9000725be786f1cf
Daerko <93711393+Daerko@users.noreply.github.com>
2022-02-11 10:47:17 +0200

    Delete Java directory

--- Java/src/univer/Final/context/Contextable.java
+++
@@ -1,8 +0,0 @@
-package univer.Final.context;
-
-import java.io.IOException;
-
-public interface Contextable {
-    void write(Object obj) throws IOException;
-    Object read() throws  IOException;
-}
--- Java/src/univer/Final/services/View.java
+++
@@ -1,7 +0,0 @@
-package univer.Final.services;
-
-public class View {
-    public static void print(String message) {
-        System.out.println(message);
-    }
-}
